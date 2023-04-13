c28086af4722f2c372e0a0e4da7e592937daf27e
beonezero <97246653+beonezero@users.noreply.github.com>
2023-04-13 19:30:34 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//document.querySelector(".title").innerText = "Hello";
+let myHeader = document.querySelector(".title");
+myHeader.innerText = "Hello Hello";
+
+let myName = document.querySelector(".name");
+myName.innerText = "Neakhai Yauheni";
+
+let plus = document.querySelector(".plus");
+plus.innerText = 66 + 33;
+
+let numberOne = 10;
+let numberTwo = 15;
+
+let mySum = numberOne + numberTwo;
+
+let sumElement = document.querySelector(".sum");
+sumElement.innerText = mySum;
+
+let multiplyElement = document.querySelector(".multiply");
+multiplyElement.innerText = 9 * 9;
+
+let minusElement = document.querySelector(".minus");
+minusElement.innerText = 100 - 35;
