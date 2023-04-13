e5e39568b89316947fbd1fff5bf03e412a83ca84
beonezero <97246653+beonezero@users.noreply.github.com>
2023-04-13 20:26:58 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,3 +21,39 @@ multiplyElement.innerText = 9 * 9;
 
 let minusElement = document.querySelector(".minus");
 minusElement.innerText = 100 - 35;
+
+let addText = document.querySelector(".add-text");
+// let firstName = "Yauheni";
+// let lastName = "Niakhai";
+// let age = 27;
+//Интерполяция
+// let fullName = `${firstName} ${lastName} ${age} - years old`;
+// addText.innerText = fullName;
+
+let person = {
+  firstName: "Yauheni",
+  lastName: "Neakhai",
+  age: 27,
+};
+
+let fullName = `${person.firstName} ${person.lastName} ${person.age}`;
+addText.innerText = fullName;
+
+let car = document.querySelector(".car");
+
+let myCar = {
+  model: "Volvo",
+  year: 2023,
+  color: "gray",
+};
+car.innerText = `Model is ${myCar.model}, color - ${myCar.color}, and year is ${myCar.year}`;
+
+let cars = ["BMW", "Audi", "Tesla", "Toyota"];
+//cars.push("Mers")
+//cars.pop();
+// cars.shift();
+// cars.unshift("Tractor");
+
+let carInner = document.querySelector(".car_inner");
+//carInner.innerText = `${cars[1]} and ${cars[3]}`;
+carInner.innerText = cars;
